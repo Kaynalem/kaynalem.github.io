13c857bd90829c624376e7168927c2832e87c2fc
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-05-08 22:13:45 -0500

    add to top button on scroll

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+toTopBtn = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        toTopBtn.style.display = "block";
+    } else {
+        toTopBtn.style.display = "none";
+    }
+}
